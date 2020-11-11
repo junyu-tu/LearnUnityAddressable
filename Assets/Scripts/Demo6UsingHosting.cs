5e7d8cf5db2d95f979cee87c025b21ae873ce382
tujunyu@rocky.games <tjy940525>
2020-11-11 17:16:08 +0800

    测试Hosting Service
    Demo6-UsingHosting

---
+++ Assets/Scripts/Demo6UsingHosting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class Demo6UsingHosting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Addressables.InstantiateAsync("whitecube");
+
+        Addressables.InstantiateAsync("redcube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
